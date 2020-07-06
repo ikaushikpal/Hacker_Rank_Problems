86298449f5f45e20db94099fa5d381d376695304
Laptop <iamkaushik@Laptop.com>
2020-07-06 20:06:40 +0530

    Day 12

---
+++ 30 Days of Code/Day 12 Inheritance.py
@@ -0,0 +1,39 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+
+class Student(Person):
+    def __init__(self,firstName, lastName, idNum, scores):
+        self.scores = scores
+        Person.__init__(self, firstName, lastName, idNum)
+    
+    def calculate(self):
+        avg = sum(self.scores)/len(self.scores)
+        if avg >= 90 and avg <= 100:
+            return 'O'
+        elif avg >= 80 and avg < 90:
+            return 'E'
+        elif avg >= 70 and avg < 80:
+            return 'A'
+        elif avg >= 55 and avg < 70:
+            return 'P'
+        elif avg >= 40 and avg < 55:
+            return 'D'
+        return 'T'
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
